150fb5888c9a052cc9e92eff19041deaf20d28ff
Grzegorz <podwikagrzegorz@gmail.com>
2021-01-01 21:34:58 +0100

    Clean code in TravelAgencyServerApplication class.

--- src/main/java/com/example/TravelAgencyServer/TravelAgencyServerApplication.java
+++ src/main/java/com/example/TravelAgencyServer/TravelAgencyServerApplication.java
@@ -1,18 +1,10 @@
 package com.example.TravelAgencyServer;
 
-import com.example.TravelAgencyServer.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TravelAgencyServerApplication {
-	private final UserService userService;
-
-	@Autowired
-	public TravelAgencyServerApplication(UserService userService) {
-		this.userService = userService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelAgencyServerApplication.class, args);
